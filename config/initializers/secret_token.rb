e1b8de8445976cbc7e0142b6549d3884a513cf71
Ian McLean <imclean@dmcouncil.org>
2016-12-02 09:51:50 -0500

    empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apeiron::Application.config.secret_key_base = '3d88966dd7b86ba405a35b03f944a76ee5537fb7c32081e5379bc45294d3e0f7de3814fd290dfa5f75b4b8ce75c7729b079a5a8456d280411ea7e1671e6ff526'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Apeiron::Application.config.session_store :cookie_store, key: '_apeiron_session'
