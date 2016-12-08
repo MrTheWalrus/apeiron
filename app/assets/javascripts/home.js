85bf72bbed449401c2eb616fa7da21662899505e
Ian McLean <imclean@dmcouncil.org>
2016-12-08 02:59:00 -0500

    Add main menu. Add logo during boot/menu. Return to menu when exiting systems view. Only initiate interlink once per boot up.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,6 +9,7 @@
   var logLimit = 12; //max number of lines in log at a time.
   var sessionId;
   var status = 'offline';
+  var interlinkActive = false;
 
   self.onDocumentReady = function() {
     initVars();
@@ -29,20 +30,17 @@
     setStatus(st, message);
   }
 
-  // TODO: Someday maybe the text doesn't live here? 
-  function bootUp(){
+  function startInterlink(){
+    setStatus('loading', 'Initialzing Apeiron Systems Interlink')
     sessionId = Math.random().toString(16).substring(2,14).toUpperCase();
-    var bootLines = [
-      ['SGCOS Modular BIOS Rev. 0102-Omega (c)2003 USAF', 1000],
-      ['Running Security Self-Check', 2000],
-      ['Checksum <span class="good">OK</span>', 250],
-      ['Input device: Keyboard: <span class="good">OK</span>', 250],
-      ['Input device: Pointer: <span class="good">OK</span>', 250],
+
+    var interlinkLines = [
+      ['Establishing interlink with Apeiron core systems.', 500],
       ['Output device: Mk5 Optical Interface Crystal: <span class="good">OK</span>', 250]
     ]
     // Half of the time, there's a problem with the OIC config and it needs to be rebuilt
     if(Math.random()>0.5){
-      $.merge(bootLines, [
+      $.merge(interlinkLines, [
         ['Input device: Mk5 Optical Interface Crystal: <span class="warn">NO_CONFIG</span>', 500],
         ['OIC configuration is stale. Generating new configuration. Please wait...', 2000],
         ['Beginning manual OIC pulse test', 1000],
@@ -54,7 +52,7 @@
         ['received 34. Sequence complete. Writing OIC config...', 800]
       ]);
     }
-    $.merge(bootLines, [
+    $.merge(interlinkLines, [
       ['Input device: Mk5 Optical Interface Crystal: <span class="good">OK</span>', 800],
       ['Running Alexandria 2.5 - Compiling opcode libraries.', 1000],
       [' + lib_apeiron_v0.11b', 600],
@@ -66,26 +64,72 @@
       ['Library build successful. 2 items, 4 dependencies.', 250],
       ['Sending Apeiron systems handshake, ID '+sessionId, 1000],
       ['Session '+sessionId+' confirmed. Interlink ready.', 600],
-      ["[<span class='key'>ESC</span>] to shut down.", 5],
-      ['Initializing Visual Interface...', 600]
+      ['Initializing Ship System Visual Interface...', 600]
     ]);
-    setStatus('booting', "Beginning system boot.")
+    logLines(interlinkLines);
+    $log.one('finished', function(){
+      interlinkActive = true;
+      setStatus('running', "System Running. Interlink active, session ID "+sessionId);
+      displaySystems();
+    })
+  }
+
+  // TODO: Someday maybe the text doesn't live here? 
+  function bootUp(){
+    var bootLines = [
+      ['SGCOS Modular BIOS Rev. 0103-Omega (c)2003 USAF', 1000],
+      ['Testing System Memory: <span class=good>256000000 bytes OK</span>', 100],
+      ['Memory Clock: <span class=good>266 MHz Tcl:5 Tcrd:7 Trp:7</span>', 300],
+      ['Running Security Self-Check', 1900],
+      ['Checksum <span class="good">OK</span>', 250],
+      ['Local Storage: <span class=good>SATA 1: ATAP1 iHAS160</span>', 200],
+      ['Input device: Keyboard: <span class="good">OK</span>', 250],
+      ['Input device: Pointer: <span class="good">OK</span>', 250],
+      ['Apeiron Interlink: <span class="bad">Offline</span>', 250],
+    ];
+    $visual.fadeIn(2000);
+    setStatus('booting', "Beginning system boot.");
     logLines(bootLines);
     $log.one('finished', function(){
-      setStatus('running', "System Online. Session ID:"+sessionId+". Interlink Active.");
-      displaySystems();
+      displayMenu();
     });
   };
+
+  function displayMenu(){
+    var menuLines = [
+      ["<span class='heading'>Main System Menu</span>", 300],
+      ["[<span class='key'>ESC</span>]: Shut down system", 100],
+      ["[<span class='key'>1</span>]: View Projects", 100]
+    ];
+    if(interlinkActive){
+      menuLines.push(["[<span class='key'>2</span>]: Apeiron Systems Interface", 5]);
+    }else{
+      menuLines.push(["[<span class='key'>2</span>]: Initiate Apeiron Interlink", 5]);
+    }
+    logLines(menuLines);
+    $log.one('finished', function(){
+      if(interlinkActive){
+        setStatus('main menu', "System Running. Interlink active, session ID "+sessionId+". Awaiting selection.");
+      }else{
+        setStatus('main menu', "System Running. Interlink offline. Awaiting selection.");
+      }
+    })
+  }
 
   function shutDown(){
     $visual.fadeOut('slow', function(){
       $visual.html('');
     });
     setStatus('shutdown', "Shutting Down");
-    logLines([
-      ['System Shutdown Initiated. Deactivating Interlink.', 2000],
-      ['Interlink offline. Goodbye...', 750]
-      ]);
+    var shutdownLines = [
+      ['System shutdown initiated.', 2000]
+    ]
+    if(interlinkActive){
+      shutdownLines.push(["Deactivating Interlink", 1000]);
+    }
+    shutdownLines.push(['Shutdown complete. Goodbye...', 750]);
+
+    logLines(shutdownLines);
     $log.one('finished', function(){
       $log.html('');
       setStatus('offline', 'System Offline. Press any key to begin boot sequence.');
@@ -93,11 +137,8 @@
   }
 
   function displaySystems(){
-    // TODO: Actual visual content?
-    $visual.html("<p>Look, if wishes were horses we'd all be eating steak.</p><p>There's nothing here.</p>");
-    $visual.load('/systems/overview', function(){
-      setStatus('systems');
-    });
+    setVisual('/systems/overview');
+    setStatus('systems');
     $visual.fadeIn('slow');
 
   }
@@ -108,17 +149,32 @@
     if(status === 'offline'){
       bootUp();
     }
-    if(status === 'booting'){
-      // debug
-      if(event.which === 13){
-        setVisual('systems/overview');
-        $log.trigger('finished');
+    if(status === 'main menu'){
+      switch(event.which){
+        case 27: //Esc
+          shutDown();
+          break;
+        case 49: //1
+          logLine('Sorry, project view is not yet implemented. Please make another selection.');
+          break;
+        case 50: //2
+          if(interlinkActive){
+            displaySystems();
+          }else{
+            startInterlink();
+          }
+          break;
       }
     }
     if(status === 'systems'){
       switch(event.which){
-        case 27: //ESC
-          shutDown();
+        case 27: //Esc
+          $visual.fadeOut('slow', function(){
+            // TODO: Image tag it.
+            $visual.html('<img alt="Sgc logo" height="300" src="/assets/sgc_logo.png">');
+            $visual.fadeIn('slow');
+          });
+          displayMenu();
           break;
         case 80: //P
           setVisual('systems/power');
