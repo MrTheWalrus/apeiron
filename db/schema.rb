a364ba2cf9dd7eb04ae8df2353d07824cdebae5a
Ian McLean <imclean@dmcouncil.org>
2016-12-09 13:05:31 -0500

    DB update, add visual angles

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20161209042722) do
     t.text     "resources"
     t.datetime "approved_at"
     t.string   "status"
-    t.string   "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
