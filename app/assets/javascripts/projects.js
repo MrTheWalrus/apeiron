7c322d0d3da0c6bdceefce6487a004c4def01ab5
Ian McLean <imclean@dmcouncil.org>
2016-12-14 14:27:10 -0500

    enable closing resource details

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -5,10 +5,14 @@
 
   self.onDocumentReady = function(){
     $('.res').click(function(){
-      console.log(this);
       $('.res-desc').show();
       // TODO: I don't think this works in IE.
+      $('.reset').show();
       $('.res-desc').html($(this).data()['description']);
+    });
+    $('.reset').click(function(){
+      $('.res-desc').hide();
+      $('.reset').hide();
     });
     $('.project-item').click(function(){
       $(this).find('.project-expand').toggle();
