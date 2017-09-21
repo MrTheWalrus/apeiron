ddde76f44a25df99e3dc192fe3c4ccf565865930
Ian McLean <imclean@dmcouncil.org>
2017-09-21 15:51:17 -0400

    numbers

--- app/assets/javascripts/systems/structural.js
+++ app/assets/javascripts/systems/structural.js
@@ -13,7 +13,7 @@
       ["Sector 4: <span class='warn'>Repairs complete.</span> Structural Integrity: <span class='good'>83%</span>", 200],
       ["Sector 5: <span class='good'>Repairs complete.</span> Structural Integrity: <span class='good'>85%</span>", 100],
       ["Sector 6: <span class='good'>All structural systems nominal.</span> Structural Integrity: <span class='good'>100%</span>", 500],
-      ["Summary: Critical structures repaired. Probabilty of surviving launch: <span class='warn'>78%</span>", 100]
+      ["Summary: Critical structures repaired. Probabilty of surviving launch: <span class='warn'>83%</span>", 100]
     ]);
     $('ul.log').one('finished', function(){
       Apeiron.home.setStatus('systems');
